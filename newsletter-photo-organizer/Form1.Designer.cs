a9ecd9118c29286ab52a13037f33cdcf6ff8737e
jimk <jim.konish@sphero.com>
2021-09-10 22:28:31 -0400

    WIP initial commit

---
+++ newsletter-photo-organizer/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace newsletter_photo_organizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageSelection = new System.Windows.Forms.TabPage();
+            this.tabPageRenaming = new System.Windows.Forms.TabPage();
+            this.tabPageOverview = new System.Windows.Forms.TabPage();
+            this.tabPageSetup = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPageSetup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageSetup);
+            this.tabControl1.Controls.Add(this.tabPageSelection);
+            this.tabControl1.Controls.Add(this.tabPageOverview);
+            this.tabControl1.Controls.Add(this.tabPageRenaming);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 463);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageSelection
+            // 
+            this.tabPageSelection.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSelection.Name = "tabPageSelection";
+            this.tabPageSelection.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSelection.Size = new System.Drawing.Size(767, 437);
+            this.tabPageSelection.TabIndex = 0;
+            this.tabPageSelection.Text = "Selection";
+            this.tabPageSelection.UseVisualStyleBackColor = true;
+            // 
+            // tabPageRenaming
+            // 
+            this.tabPageRenaming.Location = new System.Drawing.Point(4, 22);
+            this.tabPageRenaming.Name = "tabPageRenaming";
+            this.tabPageRenaming.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageRenaming.Size = new System.Drawing.Size(767, 437);
+            this.tabPageRenaming.TabIndex = 1;
+            this.tabPageRenaming.Text = "Renaming";
+            this.tabPageRenaming.UseVisualStyleBackColor = true;
+            // 
+            // tabPageOverview
+            // 
+            this.tabPageOverview.Location = new System.Drawing.Point(4, 22);
+            this.tabPageOverview.Name = "tabPageOverview";
+            this.tabPageOverview.Size = new System.Drawing.Size(767, 437);
+            this.tabPageOverview.TabIndex = 2;
+            this.tabPageOverview.Text = "Overview";
+            this.tabPageOverview.UseVisualStyleBackColor = true;
+            // 
+            // tabPageSetup
+            // 
+            this.tabPageSetup.Controls.Add(this.label1);
+            this.tabPageSetup.Controls.Add(this.comboBox1);
+            this.tabPageSetup.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSetup.Name = "tabPageSetup";
+            this.tabPageSetup.Size = new System.Drawing.Size(767, 437);
+            this.tabPageSetup.TabIndex = 3;
+            this.tabPageSetup.Text = "Setup";
+            this.tabPageSetup.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "M-W-F",
+            "T-Th"});
+            this.comboBox1.Location = new System.Drawing.Point(103, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Which class?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageSetup.ResumeLayout(false);
+            this.tabPageSetup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageSetup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPageSelection;
+        private System.Windows.Forms.TabPage tabPageOverview;
+        private System.Windows.Forms.TabPage tabPageRenaming;
+    }
+}
+
